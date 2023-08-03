9d5d440c772bb36dd45c39c13f5715e22ccebd76
Jos Warmer <jos.warmer@openmodeling.nl>
2023-08-03 20:20:58 +0200

    Lioncore M3 language

---
+++ src/webapp/config/WebappConfiguration.ts
@@ -0,0 +1,18 @@
+import type { FreEnvironment } from "@freon4dsl/core";
+import { LanguageInitializer } from "../language/LanguageInitializer";
+import type { IServerCommunication } from "../server/IServerCommunication";
+import { ServerCommunication } from "../server/ServerCommunication";
+
+
+/**
+ * The one and only reference to the actual language for which this editor runs
+ */
+import { LIonCoreM3Environment } from "../../picode/config/gen/LIonCoreM3Environment";
+export const editorEnvironment: FreEnvironment = LIonCoreM3Environment.getInstance();
+LanguageInitializer.initialize();
+
+/**
+ * The one and only reference to the server on which the models are stored
+ */
+export const serverCommunication: IServerCommunication = ServerCommunication.getInstance();
+// export const serverCommunication: IServerCommunication = MpsServerCommunication.getInstance();
