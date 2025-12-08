8dc3080071e5cec0a23bdb8009849786769154cc
Jos Warmer <jos.warmer@openmodeling.nl>
2025-12-08 13:31:48 +0100

    remove old files

--- apps/lioncore-m3-language/keep/commandline/FreonCommandLineRunner.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=FreonCommandLineRunner.d.ts.map
--- apps/lioncore-m3-language/keep/commandline/gen/DummyAction.d.ts
+++
@@ -1,8 +0,0 @@
-import { CommandLineAction, CommandLineStringParameter } from "@rushstack/ts-command-line";
-export declare class DummyAction extends CommandLineAction {
-    dummyParameter: CommandLineStringParameter;
-    constructor();
-    protected onExecute(): Promise<void>;
-    dummyAction(): Promise<string>;
-}
-//# sourceMappingURL=DummyAction.d.ts.map
